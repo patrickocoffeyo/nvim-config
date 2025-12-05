5acbaee1534d7ad0f07b9ba9077d85b1d36a316a
Patrick Coffey <patrickcoffey48@gmail.com>
2025-12-04 19:52:01 -0600

    feat: add lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  event = "VeryLazy",
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "auto",
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
+        icons_enabled = true,
+        globalstatus = true,
+      },
+
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff" },
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = { "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+    })
+  end,
+}
